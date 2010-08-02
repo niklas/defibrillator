a6986ac8f5709bbb17e10db2d688fcf25a52f260
Niklas Hofer <niklas+dev@lanpartei.de>
2010-08-02 10:28:55 +0200

    init rails 3.0.0.rc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Defibrillator::Application.config.secret_token = 'a8eabd3d2977d5625bf58c6a0dd1f30f01ee4becf8ed376455dc7a75ea0aa68f8619a09caacdcfbe3c8a0d35ed479285a8c81053b94ec75d62883262eb9977dc'
