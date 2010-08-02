89c75b6d2756e8d3c7efee0c0c8e80260b3af011
Niklas Hofer <niklas+dev@lanpartei.de>
2010-08-02 12:13:41 +0200

    author gets saved in project update

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,9 +5,11 @@ class Project < ActiveRecord::Base
 
   validates_format_of :name,:with => /\A[_\w_]+\z/
 
-  def update_attributes_from_shell(argv)
+  def update_attributes_from_shell(*argv)
     new_attr = argv.inject({}) do |hash, arg|
       key, val = arg.split(':')
+      val = $1 if val =~ %r~^"([^"]+)"$~
+      val = $1 if val =~ %r~^'([^']+)'$~
       if key && val
         hash[key] = val
       end
@@ -20,20 +22,25 @@ class Project < ActiveRecord::Base
     updates.count
   end
 
+  attr_writer :author
+
   before_update :store_changes
   after_update :persist_stored_changes
 
   private
   def store_changes
-    @update_attributes = changes.inject({}) do |upd,change|
+    @updated_attributes = changes.inject({}) do |upd,change|
       upd[change.first] = change.last.last
       upd
+    end
+    unless @updated_attributes.empty?
+      @updated_attributes['author'] = @author
     end
   end
 
   def persist_stored_changes
-    logger.debug { "updating: #{@update_attributes.inject}" }
-    updates.create! @update_attributes
+    logger.debug { "updating: #{@updated_attributes.inspect}" }
+    updates.create! @updated_attributes
   end
 
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -8,12 +8,12 @@ describe Project do
 
   describe "updating from shell" do
 
-    it 'should update own attributes' do
+    it 'should update status' do
       @project.should_receive(:update_attributes).with('status' => 'failed')
       @project.update_attributes_from_shell 'status:failed'
     end
 
-    it 'should create a project update' do
+    it 'should create a project update on status change' do
       lambda {
         @project.update_attributes_from_shell 'status:failed'
       }.should change(@project, :updates_count).by(1)
@@ -22,6 +22,25 @@ describe Project do
       @update.status.should == 'failed'
     end
 
+    it 'should create project update on status and author change (double quote)' do
+      lambda {
+        @project.update_attributes_from_shell 'status:failed','author:"Failbot <fail@example.com"'
+      }.should change(@project, :updates_count).by(1)
+
+      @update = @project.updates.last
+      @update.status.should == 'failed'
+      @update.author.should == 'Failbot <fail@example.com'
+    end
+
+    it 'should create project update on status and author change (single quote)' do
+      lambda {
+        @project.update_attributes_from_shell 'status:failed',"author:'Failbot <fail@example.com'"
+      }.should change(@project, :updates_count).by(1)
+
+      @update = @project.updates.last
+      @update.status.should == 'failed'
+      @update.author.should == 'Failbot <fail@example.com'
+    end
     
   end
 
