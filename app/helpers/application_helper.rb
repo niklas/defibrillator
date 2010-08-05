87b1c95a377e99fd99f5f12e6799667ca4e497e5
Niklas Hofer <niklas+dev@lanpartei.de>
2010-08-05 11:57:26 +0200

    link to cijoe instance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # url to the cijoe instance of the project
+  # these won't be recognized by defribillator, but must be configured in you webserver!
+  def cijoe_path(project)
+    "/status/#{project.name}"
+  end
 end
