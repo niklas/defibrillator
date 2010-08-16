05d64130425d59e08adff466511a6c4ab8e84323
Niklas Hofer <niklas+dev@lanpartei.de>
2010-08-16 11:44:37 +0200

    link to project revision

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
   # url to the cijoe instance of the project
   # these won't be recognized by defribillator, but must be configured in you webserver!
   def cijoe_path(project)
-    "/#{project.name}"
+    "/#{project.name}=#{project.revision}"
   end
 end
