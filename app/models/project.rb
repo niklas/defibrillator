d8a49c43dd94c9feb6a1a840f680f56b8fc4165b
Niklas Hofer <niklas+dev@lanpartei.de>
2010-08-16 11:29:23 +0200

    dependent => :destroy project updates

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
 
-  has_many :updates, :class_name => 'ProjectUpdate'
+  has_many :updates, :class_name => 'ProjectUpdate', :dependent => :destroy
   validates_presence_of :name
   validates_presence_of :revision
 
