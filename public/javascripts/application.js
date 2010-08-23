4b1e09d32573142157d87567e96175806a51623e
Niklas Hofer <niklas+dev@lanpartei.de>
2010-08-23 11:33:16 +0200

    latest updates in front, fading out

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,28 +4,32 @@ $(document).ready(function() {
 
     $('#projects').data('updatedAt', $('#projects').attr('updatedAt'));
 
+    var applyBehaviours = function() {
+      $('#projects .project').each(function() {
+        var $project = $(this);
+        var $updates = $project.find('.updates .update');
+        $updates.each(function(i) {
+          $(this).fadeTo(100, 1 - (i / $updates.length  ) );
+        });
+      });
+    };
+
+
     var updateProjects = function() {
       var update_path = $('#projects').attr('href');
 
       if (update_path !== undefined) {
-      
         $.ajax({
           url: update_path + "?after=" + $('#projects').data('updatedAt'),
           dataType: "script",
           complete: function(req, stat) {
-            console.debug(req);
           }
         });
-
-        // TODO put this into the attribute of #projects
-        //    if (updatedAt < update.updated_at) {
-        //      updatedAt = update.updated_at;
-        //    }
-
-
       }
     };
     setInterval( updateProjects, 5000);
+
+    applyBehaviours();
 
     // reload whole page every hour
     setTimeout( function() { location.reload(); }, 1000 * 60 * 5);
