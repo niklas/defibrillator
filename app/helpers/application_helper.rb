0f3492f5a7b1e701525c50b568cc76f676623b19
Niklas Hofer <niklas+dev@lanpartei.de>
2010-09-12 14:52:57 +0200

    show icons for authors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,20 @@ module ApplicationHelper
   def cijoe_path(project)
     "/#{project.name}=#{project.revision}"
   end
+
+  def gravatar_tag(author,opts={})
+    image_tag author.gravatar_url, opts.merge(:alt => author.name, :class => 'gravatar')
+  end
+
+  def anonymous_tag(opts={})
+    image_tag 'anonymous.png', :class => 'anonymous gravatar'
+  end
+
+  def spinner_tag(opts={})
+    image_tag 'building.gif', :class => 'spinner'
+  end
+
+  def unknown_tag(opts={})
+    image_tag 'new.png', :class => 'new'
+  end
 end
